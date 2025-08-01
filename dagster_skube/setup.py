80905f5a054af1ffe689c791b8e94cbec7e73f29
SukhmanSra <ssra5@uwo.ca>
2025-08-01 00:45:09 -0400

    upgrade

--- dagster_skube/setup.py
+++ dagster_skube/setup.py
@@ -4,7 +4,7 @@ setup(
     name="dagster_skube",
     packages=find_packages(exclude=["dagster_skube_tests"]),
     install_requires=[
-        "dagster[postgres,aws]==1.*",
+        "dagster[postgres,aws]>=1.4.0,<2.0",
         "dagster-cloud"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
